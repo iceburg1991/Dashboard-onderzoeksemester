40978148a504db4536a73739be3c7c43c1dfd853
Hugo Zonderland <info@hugozonderland.nl>
2013-05-15 10:05:23 +0200

    Added database information.

--- config.php
+++ config.php
@@ -8,21 +8,21 @@
  */
 
 // Google Console Credentials
-define('ACCESS_TYPE', 'online');
-define('APPLICATION_NAME', 'Dashboard proof of concept');
-define('CLIENT_ID', '814565602276.apps.googleusercontent.com');
-define('CLIENT_SECRET', 'EkMosZD60pI5At0yATGtgp7M');
-define('REDIRECT_URI', 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER['PHP_SELF']);
-define('DEVELOPER_KEY', 'AIzaSyBiyh3JZfDQ_ZFnx0hSKAayNG2IKzfRi4U');
+define('ACCESS_TYPE',       'online');
+define('APPLICATION_NAME',  'Dashboard proof of concept');
+define('CLIENT_ID',         '814565602276.apps.googleusercontent.com');
+define('CLIENT_SECRET',     'EkMosZD60pI5At0yATGtgp7M');
+define('REDIRECT_URI',      'http://' . $_SERVER["HTTP_HOST"] . $_SERVER['PHP_SELF']);
+define('DEVELOPER_KEY',     'AIzaSyBiyh3JZfDQ_ZFnx0hSKAayNG2IKzfRi4U');
 
 // Magento API settings
-define('API_USER', 'Hugo');
-define('API_KEY', 'Ka0yoiAOJhoifqap0oinhlkqfn0oe8vh0234gtQ965WGEWROIUHJWEROIGNRESD98OHL234TWP98YWERFGNLKAERGO87HKJN234TPHJKZWGRHLI');
-define('API_HOST', 'http://magento.presteren.nu/api/soap/?wsdl');
+define('API_USER',          'Hugo');
+define('API_KEY',           'Ka0yoiAOJhoifqap0oinhlkqfn0oe8vh0234gtQ965WGEWROIUHJWEROIGNRESD98OHL234TWP98YWERFGNLKAERGO87HKJN234TPHJKZWGRHLI');
+define('API_HOST',          'http://magento.presteren.nu/api/soap/?wsdl');
 
 //Database settings
-define('DB_HOST', '');
-define('DB_NAME', '');
-define('DB_USERNAME', '');
-define('DB_PASS', '');
+define('DB_HOST',           'localhost');
+define('DB_NAME',           'ocrtxndf_dash');
+define('DB_USERNAME',       'ocrtxndf_dashboard');
+define('DB_PASS',           'ocrtxndf_dashboard');
 ?>
