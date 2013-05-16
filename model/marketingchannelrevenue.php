9b71495509b21ff793204169e10e0bd1174a7ae7
aegis123 <dylan.drost@gmail.com>
2013-05-16 14:24:54 +0200

    made object model to be used for DB

---
+++ model/marketingchannelrevenue.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: dylan
+ * Date: 16-5-13
+ * Time: 13:36
+ * To change this template use File | Settings | File Templates.
+ */
+
+class marketingchannelrevenue extends RedBean_SimpleModel {
+    public $channel_revenue;
+    public $timestamp;
+}
---
+++ model/productquantity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: dylan
+ * Date: 16-5-13
+ * Time: 13:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+class productquantity extends RedBean_SimpleModel {
+    public  $quantity;
+}
