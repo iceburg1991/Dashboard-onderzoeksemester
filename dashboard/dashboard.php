49b4bda91a024efdb568328212f9079f46e17b7e
Hugo Zonderland <info@hugozonderland.nl>
2013-05-16 17:13:05 +0200

    Added some comments.

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -17,15 +17,19 @@ class Dashboard
     {
         // 7 day time filter
         $from = date('Y-m-d', time() - 7 * 24 * 60 * 60);
-        $to = date('Y-m-d H:00:00', time() + 2 * 60 * 60);
+        $to = date('Y-m-d H:00:00', time() + 2 * 60 * 60); // +2 hours because of the server time.
 
+        // Data
         $rows = R::getAll("SELECT mc.id AS id, mc.name AS name, SUM(mcr.channel_revenue) AS revenue FROM marketingchannel mc, marketingchannelrevenue mcr WHERE mcr.timestamp >= '" . $from . "' AND mcr.timestamp <= '". $to . "' AND mc.id = mcr.marketingchannel_id GROUP BY mc.name");
+        // Extract data
         $channels = R::convertToBeans('marketingchannelrevenue', $rows);
 
+        // Add to this array
         foreach ($channels as $channel){
             $this->totalProfitArray[$channel->name] = $channel->revenue;
         }
 
+        // Return shat
         return;
     }
 
