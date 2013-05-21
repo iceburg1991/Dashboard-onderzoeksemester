5c3d84c2592ee79747a9a5f19ea4ab5e15080aa1
aegis123 <dylan.drost@gmail.com>
2013-05-21 17:25:16 +0200

    fubar google charts api

--- dashboard/googleChart.html.php
+++ dashboard/googleChart.html.php
@@ -1,12 +1,8 @@
 
-
-
-
-
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-<script type="text/javascript" src="../js/googleChart.js"></script>
+<script type="text/javascript" src="js/googleChart.js"></script>
 
 <!--Div that will hold the pie chart-->
 <div id="chart_div">
-    <script> loadChart();</script>
+    <script> showGoogleChart(); </script>
 </div>
