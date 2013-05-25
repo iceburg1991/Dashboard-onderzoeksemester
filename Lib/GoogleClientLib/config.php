4d1ac49b5bb7bf8a999b6d93c74c54ba9d7673a3
Hugo Zonderland <info@hugozonderland.nl>
2013-05-25 14:25:51 +0200

    Include fix.

--- Lib/GoogleClientLib/config.php
+++ Lib/GoogleClientLib/config.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require_once 'config.php';
+require '././config.php';
 
 global $apiConfig;
 
